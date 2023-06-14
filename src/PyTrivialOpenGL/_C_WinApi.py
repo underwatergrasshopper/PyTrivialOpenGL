2727c1f262e1934c36505cf5876ae371ff3f2d7d
underwatergrasshopper <underwater.grasshopper@gmail.com>
2023-06-14 05:14:15 +0200

    Added portion of WinApi functions to _C_WinApi module.

--- src/PyTrivialOpenGL/_C_WinApi.py
+++ src/PyTrivialOpenGL/_C_WinApi.py
@@ -3,8 +3,14 @@ import ctypes.wintypes  as _wt
 
 ### WinApi Libraries ###
 
-_Kernel32   = _ct.windll.Kernel32
-_User32     = _ct.windll.User32
+#_Kernel32   = _ct.windll.Kernel32
+#_User32     = _ct.windll.User32
+#_Dwmapi     = _ct.windll.Dwmapi
+
+_Kernel32   = _ct.windll.LoadLibrary("C:\\Windows\\SysWOW64\\Kernel32.dll")
+_User32     = _ct.windll.LoadLibrary("C:\\Windows\\SysWOW64\\User32.dll")
+_Dwmapi     = _ct.windll.LoadLibrary("C:\\Windows\\SysWOW64\\Dwmapi.dll")
+
 
 ### WinApi Constants ###
 
@@ -14,6 +20,21 @@ SM_CYSCREEN             = 1
 
 # SystemParametersInfo, uiAction
 SPI_GETWORKAREA         = 0x0030
+
+# DwmGetWindowAttribute, dwAttribute
+DWMWA_EXTENDED_FRAME_BOUNDS = 9
+
+# GetWindow, uCmd
+GW_HWNDFIRST            = 0
+GW_HWNDLAST             = 1
+GW_HWNDNEXT             = 2
+GW_HWNDPREV             = 3
+GW_OWNER                = 4
+GW_CHILD                = 5
+GW_ENABLEDPOPUP         = 6
+
+# Errors
+ERROR_INVALID_PARAMETER = 0x57
 
 ### WinApi Types ###
 
@@ -57,11 +78,17 @@ HWND                    = _wt.HWND
 WPARAM                  = _wt.WPARAM
 LPARAM                  = _wt.LPARAM
 
+HRESULT                 = _ct.c_long
+
 ### WinApi Structures ###
 
 POINT                   = _wt.POINT
 SIZE                    = _wt.SIZE
 RECT                    = _wt.RECT
+
+LPPOINT                 = _wt.LPPOINT
+LPSIZE                  = _wt.LPSIZE
+LPRECT                  = _wt.LPRECT
 
 MSG                     = _wt.MSG
 
@@ -80,4 +107,71 @@ GetSystemMetrics        = _ct.WINFUNCTYPE(_ct.c_int, _ct.c_int)(
 SystemParametersInfoW   = _ct.WINFUNCTYPE(BOOL, UINT, UINT, PVOID, UINT)(
     ("SystemParametersInfoW", _User32), 
     ((1, "uiAction"), (1, "uiParam"), (1, "pvParam"), (1, "fWinIni"))
+)
+
+### Window ###
+
+GetForegroundWindow     = _ct.WINFUNCTYPE(HWND)(
+    ("GetForegroundWindow", _User32), 
+    ()
+)
+
+SetForegroundWindow     = _ct.WINFUNCTYPE(BOOL, HWND)(
+    ("SetForegroundWindow", _User32), 
+    ((1, "hWnd"),)
+)
+
+SetFocus                = _ct.WINFUNCTYPE(HWND, HWND)(
+    ("SetFocus", _User32), 
+    ((1, "hWnd"),)
+)
+
+FindWindowW             = _ct.WINFUNCTYPE(HWND, LPCWSTR, LPCWSTR)(
+    ("FindWindowW", _User32), 
+    ((1, "lpClassName", None), (1, "lpWindowName", None))
+)
+
+GetActiveWindow         = _ct.WINFUNCTYPE(HWND)(
+    ("GetActiveWindow", _User32), 
+    ()
+)
+
+GetWindow               = _ct.WINFUNCTYPE(HWND, HWND, UINT)(
+    ("GetWindow", _User32), 
+    ((1, "hWnd"), (1, "uCmd"))
+)
+
+GetWindowTextLengthW    = _ct.WINFUNCTYPE(_ct.c_int, HWND)(
+    ("GetWindowTextLengthW", _User32), 
+    ((1, "hWnd"),)
+)
+
+GetWindowTextW          = _ct.WINFUNCTYPE(_ct.c_int, HWND, LPWSTR, _ct.c_int)(
+    ("GetWindowTextW", _User32), 
+    ((1, "hWnd"), (1, "lpString"), (1, "nMaxCount"))
+)
+
+GetWindowRect           = _ct.WINFUNCTYPE(BOOL, HWND, LPRECT)(
+    ("GetWindowRect", _User32), 
+    ((1, "hWnd"), (1, "lpRect"))
+)
+
+GetClientRect           = _ct.WINFUNCTYPE(BOOL, HWND, LPRECT)(
+    ("GetClientRect", _User32), 
+    ((1, "hWnd"), (1, "lpRect"))
+)
+
+DwmGetWindowAttribute   = _ct.WINFUNCTYPE(HRESULT, HWND, DWORD, PVOID, DWORD)(
+    ("DwmGetWindowAttribute", _Dwmapi), 
+    ((1, "hwnd"), (1, "dwAttribute"), (1, "pvAttribute"), (1, "cbAttribute"))
+)
+
+ClientToScreen          = _ct.WINFUNCTYPE(BOOL, HWND, LPPOINT)(
+    ("ClientToScreen", _User32), 
+    ((1, "hWnd"), (1, "lpPoint"))
+)
+
+ScreenToClient          = _ct.WINFUNCTYPE(BOOL, HWND, LPPOINT)(
+    ("ScreenToClient", _User32), 
+    ((1, "hWnd"), (1, "lpPoint"))
 )
--- tests/_c_winapi_test.py
+++ tests/_c_winapi_test.py
@@ -1,6 +1,12 @@
 from PyTrivialOpenGL._C_WinApi import *
 from ctypes import *
 
+def rect_to_str(r):
+    return "%d %d %d %d" % (r.left, r.top, r.right, r.bottom)
+
+def rect_diff(a, b):
+    return RECT(a.left - b.left, a.top - b.top, a.right - b.right, a.bottom - b.bottom)
+
 if __name__ == "__main__":
     width   = GetSystemMetrics(SM_CXSCREEN)
     height  = GetSystemMetrics(SM_CYSCREEN)
@@ -15,6 +21,65 @@ if __name__ == "__main__":
     if not SystemParametersInfoW(NOT_WORKING_CODE, 0, None, 0):
         print("Error Code: %d" % GetLastError())
         print(FormatError(GetLastError()))
+        
+
+    window_handle = GetForegroundWindow()
+    print(window_handle)
+
+    window_rect = RECT()
+    GetWindowRect(window_handle, byref(window_rect))
+    print(rect_to_str(window_rect))
+
+    actual_window_rect = RECT()
+    DwmGetWindowAttribute(window_handle, DWMWA_EXTENDED_FRAME_BOUNDS, byref(actual_window_rect), sizeof(RECT))
+    print(rect_to_str(actual_window_rect))
+    print(rect_to_str(rect_diff(actual_window_rect, window_rect)))
+
+    client_rect = RECT()
+    GetClientRect(window_handle, byref(client_rect))
+    print(rect_to_str(client_rect))
+
+    points = cast(byref(client_rect), LPPOINT)
+
+    ClientToScreen(window_handle, points)
+    ClientToScreen(window_handle, byref(points[1]))
+
+    print(rect_to_str(client_rect))
+
+    ScreenToClient(window_handle, points)
+    ScreenToClient(window_handle, byref(points[1]))
+
+    print(rect_to_str(client_rect))
+
+    ### get window name ###
+
+    num_of_chars = GetWindowTextLengthW(window_handle)
+    length = num_of_chars + 1
+    buffer = (c_wchar * length)()
+    GetWindowTextW(window_handle, buffer, length)
+
+    window_name = cast(buffer, c_wchar_p).value
+    print(window_name)
+
+    ### 
+
+    window_handle = FindWindowW(None, "Untitled - Notepad")
+    if window_handle:
+        if not SetForegroundWindow(window_handle):
+            print(FormatError(GetLastError()))
+        
+        print("Window 'Untitled - Notepad' moved to foreground.")
+
+    window_handle = FindWindowW(None, "*Untitled - Notepad")
+    if window_handle:
+        if not SetForegroundWindow(window_handle):
+            print(FormatError(GetLastError()))
+        
+        print("Focused on '*Untitled - Notepad'.")
+
+
+
+    
 
 
     
