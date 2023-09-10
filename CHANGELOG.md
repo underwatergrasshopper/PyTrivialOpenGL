8834c5bc813f7fd7c463afef1b44b466c9c12182
underwatergrasshopper <underwater.grasshopper@gmail.com>
2023-09-10 17:18:18 +0200

    Added package info.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,3 +4,4 @@
 * Added full support for `pytest` to unit tests.
 * Moved setting the output path to outside of `PyTrivialOpenGL_TestsAndExamples` module.
 * Improved `ExampleManager` and `ExampleRunner` modules from `PyTrivialOpenGL_TestsAndExamples`.
+* Added `PyTrivialOpenGL` package information to the package.
--- src/PyTrivialOpenGL/__init__.py
+++ src/PyTrivialOpenGL/__init__.py
@@ -1,3 +1,13 @@
+"""
+Creates window with OpenGL Rendering Context.
+Allows to draw to window with use of OpenGL functions.
+
+To run window use `to_window().create_and_run(...)`.
+"""
+
+__author__  = "underwatergrasshopper"
+__version__ = "0.1.1"
+
 # This module and modules imported below are logically part of one module.
 # Note: They are physically separated only for convenience.
 from .Point         import *
